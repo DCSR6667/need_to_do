29480b2a2858e3a17c3feec90c38355bf1c838e6
DCSR6667 <sekharc446@example.com>
2024-03-19 20:30:14 -0400

    saving changes on 19th MAR 2024

---
+++ stack_and_queue_problems/stack_using_list_without_size_limit.py
@@ -0,0 +1,59 @@
+class stack:
+    
+    def __init__(self,size):
+        self.l=[]
+       
+        
+    def display(self):
+        if len(self.l)==0:
+            print("stack is empty")
+        else:
+            n=len(self.l)-1
+            while n>=0:
+                print(self.l[n])
+                n-=1
+            
+        
+    def isempty(self):
+        if len(self.l)==0:
+            return True
+        else:
+            return False
+            
+        
+    def push(self,ele):
+        self.l.append(ele)
+        return ele
+    
+    def pop(self):
+        if self.isempty():
+            print("stack is empty we can't delete an element")
+            return
+        return self.l.pop()
+    
+    def peek(self):
+        if self.isempty():
+            print("stack is empty")
+            return
+        return self.l[-1]
+    
+    
+st=stack(5)
+st.push(1)
+st.push(2)
+st.push(3)
+st.push(4)
+
+st.push(5)
+st.push(6)
+st.push(7)
+
+
+
+
+print(st.peek())
+    
+    
+    
+    
+        
